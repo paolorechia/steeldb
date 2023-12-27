51433ffb86c4f8d69e4543e9def568f69dd8017d
Paolo Rechia <paolorechia@gmail.com>
2023-12-28 00:54:28 +0100

    Move parse logic to steeldb-parser

--- src/database/parser.rs
+++ src/database/parser.rs
@@ -1,10 +1,6 @@
 use super::command::Command;
 use super::table::DEFAULT_TABLE;
-use steeldb_parser::select::SelectParser;
-
-pub enum ParseError {
-    Error(String),
-}
+pub use steeldb_parser::{parse_select, ParseError};
 
 pub fn parse(input: String) -> Result<Vec<Command>, ParseError> {
     let result = parse_select(input);
@@ -19,22 +15,4 @@ pub fn parse(input: String) -> Result<Vec<Command>, ParseError> {
             return Err(error);
         }
     }
-}
-
-fn parse_select(input: String) -> Result<Vec<String>, ParseError> {
-    let mut result: Vec<String> = vec![];
-    let parser = SelectParser::new();
-    let maybe_error = parser.parse(&mut result, input.as_str());
-    match maybe_error {
-        Ok(_) => {
-            return Ok(result);
-        }
-        Err(error) => {
-            let error = format!("{:?}", error);
-            return Err(ParseError::Error(format!(
-                "Failed to parse, error: {}",
-                error
-            )));
-        }
-    };
 }
--- steeldb-parser/src/lib.rs
+++ steeldb-parser/src/lib.rs
@@ -2,6 +2,29 @@ use lalrpop_util::lalrpop_mod;
 
 lalrpop_mod!(pub select); // synthesized by LALRPOP
 
+#[derive(Debug)]
+pub enum ParseError {
+    Error(String),
+}
+
+pub fn parse_select(input: String) -> Result<Vec<String>, ParseError> {
+    let mut result: Vec<String> = vec![];
+    let parser = select::SelectParser::new();
+    let maybe_error = parser.parse(&mut result, input.as_str());
+    match maybe_error {
+        Ok(_) => {
+            return Ok(result);
+        }
+        Err(error) => {
+            let error = format!("{:?}", error);
+            return Err(ParseError::Error(format!(
+                "Failed to parse, error: {}",
+                error
+            )));
+        }
+    };
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -19,7 +42,9 @@ mod tests {
     fn test_select_parser_multiple_columns() {
         let mut result: Vec<String> = vec![];
         let parser = select::SelectParser::new();
-        parser.parse(&mut result, "select brigadeiro, churros;").unwrap();
+        parser
+            .parse(&mut result, "select brigadeiro, churros;")
+            .unwrap();
         let v = vec!["brigadeiro".to_string(), "churros".to_string()];
         assert_eq!(v, result);
     }
@@ -30,5 +55,12 @@ mod tests {
         let parser = select::SelectParser::new();
         assert!(parser.parse(&mut result, "select *;").is_ok());
         assert_eq!(result, vec!["*".to_string()]);
+    }
+
+    #[test]
+    fn test_parse_select() {
+        let result = parse_select("select brigadeiro, churros;".to_string()).unwrap();
+        let v = vec!["brigadeiro".to_string(), "churros".to_string()];
+        assert_eq!(v, result);
     }
 }
