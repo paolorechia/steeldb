823ba9c219c57dc7c013761800f49a707efdcac8
Paolo Rechia <paolorechia@gmail.com>
2023-12-28 00:22:50 +0100

    Table printing

--- src/repl.rs
+++ src/repl.rs
@@ -2,7 +2,7 @@ use crate::database::datatypes::DataType;
 use crate::database::steeldb::{ExecutionResult, SteelDB};
 use crate::database::table::Table;
 use crate::VERSION;
-use core::num;
+use std::collections::HashMap;
 use std::io;
 use std::io::Write;
 
@@ -44,27 +44,100 @@ impl Repl {
         println!("");
     }
 
-    fn print_table_fields(&self, column_names: Vec<String>) {
+    fn print_table_fields(&self, column_names: &Vec<String>) -> HashMap<String, i32> {
+        let mut column_widths = HashMap::<String, i32>::new();
         print!("|");
         for i in 0..column_names.len() as i32 {
+            let mut column_width = 0;
             let column = column_names.get(i as usize).unwrap();
             for _ in 0..self.padding {
                 print!(" ");
+                column_width += 1;
             }
             print!("{}", column);
+            column_width += column.len() as i32;
             for _ in 0..self.padding - 1 {
                 print!(" ");
+                column_width += 1;
             }
             if i < column_names.len() as i32 - 1 {
                 print!("|");
             } else {
                 print!(" ");
             }
+            column_widths.insert(column.clone(), column_width);
         }
         println!("|");
+        return column_widths;
     }
 
-    fn print_table_columns(&self, table: &Table) {}
+    fn print_table_columns(
+        &self,
+        table: &Table,
+        column_names: &Vec<String>,
+        number_rows: i32,
+        column_widths: HashMap<String, i32>,
+        names_length: i32,
+    ) {
+        for i in 0..number_rows {
+            print!("|");
+            for j in 0..column_names.len() as i32 {
+                let col_name = column_names.get(j as usize).unwrap();
+                let col = table.columns.get(col_name).unwrap();
+
+                let maybe_value = col.get(i as usize);
+                let size_of_value: i32;
+                let mut val = "".to_string();
+
+                match maybe_value {
+                    // Have value for this row
+                    Some(value) => match value {
+                        DataType::String(string_value) => {
+                            size_of_value = string_value.len() as i32;
+                            val = string_value.clone();
+                        }
+                        DataType::Float32(float_value) => {
+                            val = float_value.to_string();
+                            size_of_value = val.len() as i32;
+                        }
+                        DataType::Integer32(integer_value) => {
+                            val = integer_value.to_string();
+                            size_of_value = val.len() as i32;
+                        }
+                    },
+                    // Missing value case
+                    None => size_of_value = 0,
+                }
+
+                let column_width = column_widths.get(col_name).unwrap();
+                let padding_size = column_width - size_of_value;
+                let mut half_padding = padding_size / 2;
+
+                for _ in 0..half_padding {
+                    print!(" ");
+                }
+
+                // If column width is odd
+                // We have one extra space since division by integer rounds down
+                if padding_size % 2 != 0 {
+                    half_padding += 1;
+                }
+
+                print!("{}", val);
+                for _ in 0..half_padding {
+                    print!(" ");
+                }
+
+                if j < column_names.len() as i32 - 1 {
+                    print!("|");
+                } else {
+                    print!(" ");
+                }
+            }
+            println!("|");
+            self.print_separator_line(column_names.len() as i32, names_length)
+        }
+    }
 
     fn print_separator_line(&self, number_columns: i32, names_length: i32) {
         let size = self.padding * 2 * number_columns + names_length;
@@ -91,7 +164,7 @@ impl Repl {
 
         // get number of rows from first column
         match maybe_column {
-            Some((key, value)) => {
+            Some((key, _)) => {
                 let col = table.columns.get(key);
                 if let Some(vec) = col {
                     number_rows = vec.len() as i32;
@@ -110,11 +183,18 @@ impl Repl {
 
         self.print_separator_line(number_columns, names_length);
 
-        self.print_table_fields(column_names);
-        if !is_empty {
-            self.print_table_columns(&table);
-        }
+        let column_widths = self.print_table_fields(&column_names);
         self.print_separator_line(number_columns, names_length);
+
+        if !is_empty {
+            self.print_table_columns(
+                &table,
+                &column_names,
+                number_rows,
+                column_widths,
+                names_length,
+            );
+        }
         io::stdout().flush().unwrap();
     }
 
