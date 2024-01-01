dbda208de229eabe2abcaac707505034fbb31eae
Paolo Rechia <paolorechia@gmail.com>
2024-01-01 19:24:28 +0100

    More docs

--- src/database/table.rs
+++ src/database/table.rs
@@ -5,24 +5,45 @@ use std::collections::HashMap;
 use std::fs::OpenOptions;
 use std::path::Path;
 
+/// This defines a way to keep the data in-memory by the SteelDB.
+/// It also represents the Table that the user receives back when querying the database.
+/// This is currently in a columnar format.
+/// Most of the exposed functionality here is a low level API meant to be used during the
+/// database development. It is not meant to be used directly by database users.
 #[derive(Debug)]
 pub struct Table {
+    /// The table name, this is used as an identifier for retrieving the correct table.
     pub name: String,
+    /// The table fields or schema.
     pub fields: HashMap<String, DataType>,
+    /// The actual data stored in columnar format.
     pub columns: HashMap<String, Vec<DataType>>,
+    /// Used retrieving data, allowing for projection push-down on query.
+    /// That is, the Database do not columns that were not specified in the query.  
     pub select_columns: Vec<String>,
 }
 
+/// The defined errors that might occur when loading or saving a table.
+/// This is forwarded back by the VirtualMachine.
 #[derive(Debug)]
 pub enum TableErrors {
+    /// The table with the given name was not found.
     TableNotFound,
+    /// Attempted to save a table with a name that already exists.
     TableAlreadyExists,
+    /// The select column was not found in the table.
     ColumnNotFound(String),
+    /// Unspecified write error when saving the table.
     WriteError(String),
+    /// Unspecified read error when loading the table.
     ReadError(String),
+    /// Generic unspecified error.
     Error(String),
 }
 
+/// Defines how the table should be saved.
+/// This is a low level API and not meant to used directly
+/// by database users.  
 pub enum SaveMode {
     Overwrite,
     Append,
@@ -33,19 +54,20 @@ fn format_column_not_found(column_name: &String) -> String {
 }
 
 impl Table {
+    /// Creates the data dir if it does not yet exist.
     pub fn init_data_dir() {
         if !Path::new(DATA_DIR).exists() {
             let result = std::fs::create_dir_all(DATA_DIR);
             result.unwrap();
         }
     }
-
+    /// Resolves the table file path based on it's name and format.
     pub fn get_table_path(name: &String, format: &FileFormat) -> String {
         match format {
             FileFormat::SimpleColumnar => format!("{}/{}.columnar", DATA_DIR, name),
         }
     }
-
+    /// Saves the table to disk.
     pub fn save(&self, mode: SaveMode, format: FileFormat) -> Result<(), TableErrors> {
         let s = Table::get_table_path(&self.name, &format);
         let path = Path::new(&s);
@@ -91,7 +113,7 @@ impl Table {
         }
         return Ok(());
     }
-
+    /// Loads the table from disk.
     pub fn load(
         table_name: String,
         select_columns: Vec<String>,
